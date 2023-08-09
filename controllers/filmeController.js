769550bc39e77e12c57976c69dcc3c5f238e033a
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-08-09 01:06:28 -0300

    feat: added 'Filmes CREATE

--- controllers/filmeController.js
+++ controllers/filmeController.js
@@ -1,7 +1,25 @@
-const {Filme: FilmeModel} = require("../models/Filme");
+const { Filme: FilmeModel } = require("../models/Filme");
 
 const filmeController = {
 
-}
+    create: async (req, res) => {
+        try {
+
+            const filme = {
+                name: req.body.name,
+                description: req.body.description,
+                year: req.body.year,
+                cover: req.body.cover,
+            };
+
+            const response = await FilmeModel.create(filme);
+
+            res.status(201).json({ response, msg: "Filme criado com sucesso!" });
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+};
 
 module.exports = filmeController;
--- routes/filmes.js
+++ routes/filmes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const filmeController = require('../controllers/filmeController');
 
-// functions
+router
+    .route("/filmes")
+    .post((req, res) => filmeController.create(req, res));
 
 module.exports = router;
