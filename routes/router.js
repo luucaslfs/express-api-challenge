60740c8aae8adc64ca10028547941c3d8bcda6fc
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-08-10 02:58:45 -0300

    feat: added User routes setup

--- routes/router.js
+++ routes/router.js
@@ -6,7 +6,10 @@ const filmesRouter = require("./filmes");
 // Series routes
 const seriesRouter = require("./series");
 
+const userRouter = require("./users");
+
 router.use("/", filmesRouter);
 router.use("/", seriesRouter);
+router.use("/", userRouter);
 
 module.exports = router;
