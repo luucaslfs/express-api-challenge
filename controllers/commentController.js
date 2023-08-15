9a7ffb60d1636aa6ba7ba561733e2d216d5aa9bd
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-08-15 17:02:22 -0300

    feat: upgraded commentController with getCommentsByID

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,30 +1,40 @@
-// controllers/CommentController.js
 const { Comment: CommentModel } = require("../models/Comment");
-const { Filme: FilmeModel } = require("../models/Filme");
+const { Content: ContentModel } = require("../models/Content");
 
 const commentController = {
+  create: async (req, res) => {
+    try {
+      const comment = {
+        user: req.user.userId,
+        text: req.body.text,
+        content: req.params.id,
+      };
 
-    create: async (req, res) => {
-        try {
-            const comment = {
-                user: req.user.userId, // Supondo que o user é anexado ao req após autenticação JWT
-                text: req.body.text,
-                filme: req.params.id,
-            };
+      const newComment = await CommentModel.create(comment);
 
-            const newComment = await CommentModel.create(comment);
+      await ContentModel.findByIdAndUpdate(req.params.id, {
+        $push: { comments: newComment._id },
+      });
 
-            // Opcional: Se você decidir manter a referência no modelo Filme
-            await FilmeModel.findByIdAndUpdate(req.params.id, {
-                $push: { comments: newComment._id }
-            });
-
-            res.status(201).json({ newComment, msg: "Comentário criado com sucesso!" });
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ msg: "Erro ao criar comentário." });
-        }
+      res
+        .status(201)
+        .json({ newComment, msg: "Comentário criado com sucesso!" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ msg: "Erro ao criar comentário." });
     }
-}
+  },
+  getCommentsByContent: async (req, res) => {
+    try {
+      const comments = await CommentModel.find({
+        content: req.params.id,
+      }).populate("user", "username"); // populate() is used to get the user's username
+      res.status(200).json(comments);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ msg: "Erro ao buscar comentários." });
+    }
+  },
+};
 
 module.exports = commentController;
