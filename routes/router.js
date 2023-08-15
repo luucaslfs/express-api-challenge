fc58653c86d7a356b4f0259de37319e08f298c41
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-08-15 16:27:25 -0300

    refactor: deleting old Filme and Serie files

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,9 @@
 const router = require("express").Router();
 
 // calling routes
-const filmesRouter = require("./filmes");
-const seriesRouter = require("./series");
 const userRouter = require("./users");
 const contentRouter = require("./contents");
 
-router.use("/", filmesRouter);
-router.use("/", seriesRouter);
 router.use("/", userRouter);
 router.use("/", contentRouter);
 
