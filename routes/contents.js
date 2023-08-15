762def4010f5daa6c7749042ae48b084b9987df2
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-08-15 17:03:51 -0300

    feat: added comment and rating routes

--- routes/contents.js
+++ routes/contents.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 
 const verifyToken = require("../middlewares/verifyToken");
 const contentController = require("../controllers/contentController");
-// If you have a commentController, you can include it as well
+const commentController = require("../controllers/commentController");
+const ratingController = require("../controllers/ratingController");
 
 router
   .route("/contents")
@@ -14,12 +15,17 @@ router.route("/contents/filmes").get(contentController.getAllMovies);
 router.route("/contents/series").get(contentController.getAllSeries);
 
 router
+  .route("/contents/:id/comments")
+  .post(verifyToken, commentController.create)
+  .get(commentController.getCommentsByContent);
+
+router.route("/contents/:id/ratings").post(verifyToken, ratingController.rate);
+
+router
   .route("/contents/:id")
   .get(contentController.getById)
-  .delete(verifyToken, contentController.delete) // Assuming you want to protect the delete route with a token
-  .put(verifyToken, contentController.update) // Assuming you want to protect the update route with a token
-  .patch(verifyToken, contentController.patch); // Assuming you want to protect the patch route with a token
-
-// If you have routes related to the commentController, you can define them here as well
+  .delete(verifyToken, contentController.delete)
+  .put(verifyToken, contentController.update)
+  .patch(verifyToken, contentController.patch);
 
 module.exports = router;
