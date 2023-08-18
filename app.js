11103343553e15f640343573fa5b8fa6f174f709
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2023-08-17 22:22:24 -0300

    refactor: added dotenv config to the top of app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
 require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
 app.use(cors());
 
